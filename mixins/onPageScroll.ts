d2b8f65628ee645b50ee04444f48605135414808
MrBaoquan <mrma617@gmail.com>
2025-02-20 18:50:55 +0800

    优化onPageScroll.

--- mixins/onPageScroll.ts
+++ mixins/onPageScroll.ts
@@ -1,8 +1,17 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 export default {
+    data: {
+        navBarOpacity: 0,
+    },
+
     onPageScroll({ scrollTop }) {
         if (__mpx_mode__ !== 'wx') return;
         const $refs = (this as any).$refs;
         $refs.navBar?.setPageScrollTop(scrollTop);
+        const that = this as any;
+
+        that.setData({
+            navBarOpacity: $refs.navBar.renderOpacity,
+        });
     },
 };
